8099d34177ae1e9e1e962ce6d5631b3633f14704
JinShiyin <shiyinjin@foxmail.com>
2022-04-26 11:46:02 +0800

    add flag and image_name_pattern

--- lib/utils/demo_utils.py
+++ lib/utils/demo_utils.py
@@ -18,8 +18,9 @@ import os
 import cv2
 import subprocess
 
-def video_to_images(vid_file, data_folder=None, return_info=False):
-    img_folder = os.path.join(data_folder, os.path.basename(vid_file).replace('.', '_'))
+def video_to_images(vid_file, data_folder=None, return_info=False, flag=None):
+    img_folder = os.path.join(data_folder, os.path.basename(vid_file).replace('.', '_')) + flag
+    print(f'{vid_file} will save frames to {img_folder}')
     if not os.path.exists(img_folder):
         os.makedirs(img_folder, exist_ok=True)
         command = ['ffmpeg',
@@ -41,11 +42,13 @@ def video_to_images(vid_file, data_folder=None, return_info=False):
         return img_folder
 
 
-def images_to_video(img_folder, output_vid_file):
+def images_to_video(img_folder, output_vid_file, image_name_pattern=None):
     os.makedirs(img_folder, exist_ok=True)
 
+    if image_name_pattern is None:
+        image_name_pattern = f'%06d.png'
     command = [
-        'ffmpeg', '-y', '-threads', '16', '-i', f'{img_folder}/%06d.png', '-profile:v', 'baseline',
+        'ffmpeg', '-y', '-threads', '16', '-i', f'{img_folder}/'+image_name_pattern, '-profile:v', 'baseline',
         '-level', '3.0', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-an', '-v', 'error', output_vid_file,
     ]
 
