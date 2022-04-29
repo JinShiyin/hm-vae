0e6c4ec361d2f1eb5762754b63323dea29fdac47
JinShiyin <shiyinjin@foxmail.com>
2022-04-29 19:10:49 +0800

    merge videos

--- merge_videos.py
+++ merge_videos.py
@@ -17,19 +17,21 @@ def test():
         'outdoors_freestyle_01.mp4', 
         'downtown_weeklyMarket_00.mp4'
     ]
-    ori_video_dir = 'outputs/input_videos'
-    merged_video_dir = 'outputs/render/merged_videos/videos/without_ori_video'
+    ori_video_dir = '/data/jsy/code/hm-vae/outputs/input_videos'
+    merged_video_dir = '/data/jsy/code/hm-vae/outputs/render/merged_videos/videos/without_ori_video'
     os.makedirs(merged_video_dir, exist_ok=True)
 
-    refined_video_dir = 'outputs/render/rendered_videos/refined/without_ori_video'
-    vibe_video_dir = 'outputs/render/rendered_videos/vibe/without_ori_video'
+    # refined_video_dir = 'outputs/render/rendered_videos/refined/without_ori_video'
+    refined_video_dir = '/data/jsy/code/hm-vae/outputs/train/len_64_no_aug_mvae/20220428184848-len_64_no_aug_mvae/images/340000/refined_video'
+    vibe_video_dir = '/data/jsy/code/hm-vae/outputs/render/rendered_videos/vibe/without_ori_video'
 
     for video_name in video_name_list:
         video_basename = os.path.splitext(video_name)[0]
         video_path_list = []
         video_path_list.append(os.path.join(ori_video_dir, video_name))
         video_path_list.append(os.path.join(vibe_video_dir, f'{video_basename}_rendered.mp4'))
-        video_path_list.append(os.path.join(refined_video_dir, f'{video_basename}_rendered.mp4'))
+        # video_path_list.append(os.path.join(refined_video_dir, f'{video_basename}_rendered.mp4'))
+        video_path_list.append(os.path.join(refined_video_dir, f'{video_basename}.mp4'))
         tag_list = ['Input', 'VIBE', 'Ours']
         output_video_path = os.path.join(merged_video_dir, f'{video_basename}_merged.mp4')
         merge_videos(
